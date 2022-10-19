d78f0107fb7597f7a227c1dc18fced79f45d32ad
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-10-20 00:10:14 +0300

    Exercise 13.20

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ Blog.belongsTo(User)
 //Blog.sync({ alter: true })
 //User.sync({ alter: true })
 
+//User.belongsToMany(Blog, { through: Reading })
+Blog.belongsToMany(User, { through: Reading })
 module.exports = {
   Blog, User, Reading
 }
